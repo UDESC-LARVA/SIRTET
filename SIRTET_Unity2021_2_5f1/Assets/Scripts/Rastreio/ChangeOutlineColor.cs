5e9dc00caf23acd48ba7ff181741d39d1e3024e3
DiegoTondorf <42287053+DiegoTondorf@users.noreply.github.com>
2024-03-22 16:40:58 -0300

    Novas implementações
    Novos atalhos para tirar elementos
    Sistema de cores de pele para dois dos 4 personagens
    Novo modelo de personagem
    Sistema de controle de arquivo de desafios

--- SIRTET_Unity2021_2_5f1/Assets/Scripts/Rastreio/ChangeOutlineColor.cs
+++ SIRTET_Unity2021_2_5f1/Assets/Scripts/Rastreio/ChangeOutlineColor.cs
@@ -4,58 +4,70 @@ using UnityEngine;
 
 public class ChangeOutlineColor : MonoBehaviour
 {
-   
+    
+
+    PlayerCharacterController pcc;
+    GameObject[] bodyPartsToChange = new GameObject[2];
+    public Material outlineMat;
+
+    void Start () 
+    {
+		pcc = FindObjectOfType<PlayerCharacterController>();
+
+        switch(this.tag)
+        {
+            case "Left_Arm":
+                bodyPartsToChange[0] = pcc.anteBracoEsq.GetChild(0).gameObject;                    
+                bodyPartsToChange[1] = pcc.bracoEsq.GetChild(0).gameObject;
+            break;
+            case "Right_Arm":
+                bodyPartsToChange[0] = pcc.anteBracoDir.GetChild(0).gameObject;                    
+                bodyPartsToChange[1] = pcc.bracoDir.GetChild(0).gameObject;
+            break;
+            case "Left_Foot":
+                bodyPartsToChange[0] = pcc.canelaEsq.GetChild(0).gameObject;                    
+                bodyPartsToChange[1] = pcc.pernaEsq.GetChild(0).gameObject;
+            break;
+            case "Right_Foot":
+                bodyPartsToChange[0] = pcc.canelaDir.GetChild(0).gameObject;                    
+                bodyPartsToChange[1] = pcc.pernaDir.GetChild(0).gameObject;
+            break;
+            case "Head":
+                bodyPartsToChange[0] = pcc.cabeca.GetChild(0).gameObject;                    
+                bodyPartsToChange[1] = pcc.cabeca.GetChild(0).gameObject;
+            break;                           
+        }
+
+        outlineMat = Resources.Load("Material/MatOutlineHit") as Material;
+	}
+
+
 	// Para colisoes
 	IEnumerator OnCollisionEnter (Collision Target)
     {	
 		if(Target.transform.tag == "Obj" && this.tag != "Untagged")
 		{
-           
-            PlayerCharacterController pcc = FindObjectOfType<PlayerCharacterController>();
-         
-            //Desta forma não é otimizado, pois a cada colisão é feito várias pesquisas o que acaba pesando o jogo
+            string tipoObjeto =  Target.gameObject.GetComponent<ObjectBehavior>().objeto.Type;
 
-	 		Transform[] bodyPartsToChange = new Transform[2];
- 
-            switch(this.tag)
+            outlineMat.color = new Color(0,0,1,0.5f);
+
+			if(tipoObjeto == "Obstaculo" || tipoObjeto == "ObstaculoR")
+				outlineMat.color = new Color(1,0,0,0.5f);
+			
+			foreach (GameObject part in bodyPartsToChange) 
             {
-                case "Left_Arm":
-                    bodyPartsToChange[0] = pcc.anteBracoEsq.transform;                    
-                    bodyPartsToChange[1] = pcc.bracoEsq.transform;
-                break;
-                case "Right_Arm":
-                    bodyPartsToChange[0] = pcc.anteBracoDir.transform;                    
-                    bodyPartsToChange[1] = pcc.bracoDir.transform;
-                break;
-                case "Left_Foot":
-                    bodyPartsToChange[0] = pcc.canelaEsq.transform;                    
-                    bodyPartsToChange[1] = pcc.pernaEsq.transform;
-                break;
-                case "Right_Foot":
-                    bodyPartsToChange[0] = pcc.canelaDir.transform;                    
-                    bodyPartsToChange[1] = pcc.pernaDir.transform;
-                break;
-                case "Head":
-                    bodyPartsToChange[0] = pcc.cabeca.transform;                    
-                    bodyPartsToChange[1] = pcc.cabeca.transform;
-                break;
-                               
+			    part.gameObject.layer = 8;
             }
-            
-
-			Color color = new Color(0,0,1,0.5f);
-			if(Target.gameObject.GetComponent<ObjectBehavior>().objeto.Type == "Obstaculo" || Target.gameObject.GetComponent<ObjectBehavior>().objeto.Type == "ObstaculoR")
-				color = new Color(1,0,0,0.5f);
-			
-			
-			foreach (Transform item in bodyPartsToChange) {
-				item.GetComponentInChildren<Renderer>().materials[1].color = color;			}
 
 			
 			yield return new WaitForSeconds(3);
-			foreach (Transform item in bodyPartsToChange) {
-				item.GetComponentInChildren<Renderer>().materials[1].color = new Color(0,1,0,0.5f);
-			}
+            foreach (GameObject part in bodyPartsToChange) 
+            {
+			    part.gameObject.layer = 7;
+            }
+
+            
+            outlineMat.color  = new Color(0,1,0,0.5f);	
 			
 		}
 	}
